fa0e49234b760a16811d7193a3c110dc696ed4eb
Abigiya Tadesse <Abigiyatadesse23@gmail.com>
2022-11-16 01:57:57 +0300

    Fixed linter error

--- Teacher.rb
+++ Teacher.rb
@@ -1,12 +1,12 @@
-require "./Person.rb"
+require './person'
 
 class Teacher < Person
-    def initialize(age, specialization, name, parent_permission)
-        super(age, name, parent_permission)
-        @specialization = specialization
-    end
+  def initialize(age, specialization, name, parent_permission)
+    super(age, name, parent_permission)
+    @specialization = specialization
+  end
 
-    def can_use_services?
-        true
-    end
+  def can_use_services?
+    true
+  end
 end
