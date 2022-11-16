f7d2ed6773e15ff4cda8c85b8429690d17253493
Abigiya Tadesse <abigiyatadesse23@gmail.com>
2022-11-16 22:34:02 +0300

    Update test.rb

--- test.rb
+++ test.rb
@@ -4,9 +4,9 @@ require './trimmer_decorator'
 
 person = Person.new(22, name: 'maximilianus')
 person.correct_name
-capitalizedPerson = CapitalizeDecorator.new(person)
-capitalizedPerson.correct_name
-capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
-capitalizedTrimmedPerson.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalizedPerson)
+capitalized_trimmed_person.correct_name
 
 # puts person.correct_name
