988f514a598891c5b3d71654ced51d2efea85717
Abigiya Tadesse <abigiyatadesse23@gmail.com>
2022-11-17 00:44:36 +0300

    Update trimmer_decorator.rb

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -1,8 +1,6 @@
 require './base_decorator'
 class TrimmerDecorator < BaseDecorator
   def correct_name
-    return @nameable.correct_name[0..9] if @nameable.correct_name.length < 10
-
-    @nameable.correct_name
+    @nameable.correct_name[0...10]
   end
 end
